25f9474a6687e247d698a08efb944ee368a92084
François Delbrayelle <fdelbrayelle@gmail.com>
2025-09-09 11:16:48 +0200

    fix(): add missing category to AI

---
+++ src/main/java/io/kestra/plugin/perplexity/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "Perplexity plugin",
+    description = "A plugin to use Perplexity.",
+    categories = PluginSubGroup.PluginCategory.AI
+)
+package io.kestra.plugin.perplexity;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
